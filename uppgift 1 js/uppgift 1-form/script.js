237b4c6b343294bc649066ce4e626501ec70cf0e
Shimo Ryuu <Mr.Toastie@hotmail.com>
2021-01-30 15:20:54 +0100

    uppgift 1

--- uppgift 1 js/uppgift 1-form/script.js
+++ uppgift 1 js/uppgift 1-form/script.js
@@ -1,34 +1,62 @@
-const from = document.querySelector('#form'); // referens till vårt formulär
+const form = document.querySelector('#form'); // referens till vårt formulär
+ //referenser till vårat output som vi ska använda
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const email = document.querySelector('#email');
 const output = document.querySelector('#output'); //referenser till vårat output som vi ska använda
 
 
+let users = [];
 
-const validate = id => {
+// const addUser = (output) => {
+
+//     output.innerHTML = '';
+//     users.forEach(user => {
+//         let html = `
+//         <div class="user">
+//                 <div class="text">
+//                     <h3>${user.firstName} ${user.lastName}</h3>
+//                     <small>${user.email}</small>
+//                 </div>
+//                 <div class="buttons">
+//                     <button class="btn btn-secondary">change</button>
+//                     <button class="btn btn-danger">delete</button>
+//                 </div>
+//                 </div>
+//     `
+//     output.innerHTML += html
+//     })
+// }
+
+
+const validateText = id => {
     const input = document.querySelector('#'+id);
     const error = document.querySelector('#'+id+'-error');
 
-    
-
-    if(input.type === "text") {
-   
-        if(input.value === '') {
-            error.innerText = 'You have to write a name!';
-            return false;
-        }
-        else if (input.value.length <2) {
-            error.innerText = 'Your name must be at least 2 letters!';
-            return false;
-        }
-        else {
-            error.innerText = '';
-            return true;
-        }
+    if(input.value === '') {
+        error.innerText = 'You have to write a name!';
+        return false;
     }
 
-    if(input.type === "email") {
+    else if (input.value.length < 3) {
+        error.innerText = 'Your name must be at least 3 letters!';
+        return false;
+    }
 
-    
-        let regEx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+    else {
+        error.innerText = '';
+        return true;
+    }
+
+}
+
+
+
+const validateEmail = id => {
+    const input = document.querySelector('#'+id);
+    const error = document.querySelector('#'+id+'-error');
+
+    let regEx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
 
         if(regEx.test(input.value)) {
             error.innerText = '';
@@ -38,24 +66,80 @@ const validate = id => {
             error.innerText = 'You have to use a valid email!'
             return false
         }
-        
-    }
-    
-
 }
+
+
+const validate = () => {
+
+    document.querySelectorAll('input').forEach(input => {
+
+        if(input.type === "text") {
+            validateText(input.id);
+        }
+        if(input.type === "email") {
+            validateEmail(input.id);
+        }
+    })
+}
+
+function listUsers() {
+    output.innerHTML = ''
+    users.forEach(user => {
+      output.innerHTML += `
+                 <div class="text">
+                     <h3>${user.firstName} ${user.lastName}</h3>
+                    <small>${user.email}</small>
+                </div>
+                <div class="buttons">
+                    <button class="btn btn-secondary">change</button>
+                   <button class="btn btn-danger">delete</button>
+                 </div>
+                </div>
+                `
+    })
+}
+
+const createUser = (firstName, lastName, email) => {
+    let user = {
+        id: Date.now().toString(),
+        firstName,
+        lastName,
+        email
+    }
+
+
+    users.push(user);
+    console.log(users);
+}
+
+// addUsers();
+
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    validate();
+
+    if(validateText('firstName') && validateText('lastName') && validateEmail('email')) {
+        createUser(firstName.value, lastName.value, email.value);
+
+        listUsers();
+    //    addUsers();
+    }
+
+    
     //selector all och for each gör att valideringer körs på varje input
-    document.querySelectorAll('input').forEach(input => {
-        validate(input.id);
-        console.log(input.id);
-    })
+    // document.querySelectorAll('input').forEach(input => {
+    //     validate(input.id);
+    // })
 
 })
+
+
+// console.log(input.id);
 
 //om man skriver ('#'+id) så kan man använda detta sätt.
 //annars validate('#firstName') eller ('firstname.id')
 // validate('firstName')
+
